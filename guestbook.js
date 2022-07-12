07d175caf5ba2a62c75be1c01efdb6e93c3888b6
ChikangaTakudzwa <69901316+ChikangaTakudzwa@users.noreply.github.com>
2022-07-12 12:14:08 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://54c08f4e.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
